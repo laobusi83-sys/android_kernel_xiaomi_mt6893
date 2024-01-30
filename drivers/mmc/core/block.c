0870b29c53eb043c514db26a52f7611f61b6b1f9
SamarV-121 <samar@samarv121.dev>
2024-01-30 21:05:08 +0530

    fixup! mmc: rpmb: fixes pause retune on all RPMB partitions.
    Change-Id: I32399d04d9f715fc781bff1a4e4b7257ccd13fa2

--- drivers/mmc/core/block.c
+++ drivers/mmc/core/block.c
@@ -1303,7 +1303,7 @@ static int mmc_blk_part_switch_pre(struct mmc_card *card,
 		if (ret)
 			return ret;
 	}
-	if (part_type == EXT_CSD_PART_CONFIG_ACC_RPMB)
+	if ((part_type & mask) == mask)
 		mmc_retune_pause(card->host);
 #else
 	if ((part_type & mask) == mask) {
@@ -1326,7 +1326,7 @@ static int mmc_blk_part_switch_post(struct mmc_card *card,
 
 
 #if defined(CONFIG_MTK_EMMC_CQ_SUPPORT) || defined(CONFIG_MTK_EMMC_HW_CQ)
-	if (part_type == EXT_CSD_PART_CONFIG_ACC_RPMB)
+	if ((part_type & mask) == mask)
 		mmc_retune_unpause(card->host);
 
 	/* enable cmdq
