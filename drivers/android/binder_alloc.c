22ce1285455fa955dd207466705b6fce1824e6c4
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-08-15 14:18:57 +0000

    binder: Fix redefinition of enumerator `BINDER_DEBUG_USER_ERROR`

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -48,7 +48,6 @@ enum {
 	BINDER_DEBUG_OPEN_CLOSE             = 1U << 1,
 	BINDER_DEBUG_BUFFER_ALLOC           = 1U << 2,
 	BINDER_DEBUG_BUFFER_ALLOC_ASYNC     = 1U << 3,
-	BINDER_DEBUG_USER_ERROR             = 1U << 4,
 };
 static uint32_t binder_alloc_debug_mask = BINDER_DEBUG_USER_ERROR;
 module_param_named(debug_mask, binder_alloc_debug_mask,
