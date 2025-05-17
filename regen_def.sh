a7cb59e231655ddc18c337acd8c0346601f73f99
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-05-17 22:33:56 +0000

    build: Add  common regen script logic * Add Support for agate as well

--- regen_def.sh
+++ regen_def.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-if [ $1 = "--ares" ]; then
-		export DEVICE=ares
-		export DEFCONFIG=ares_user_defconfig
-	else
-		export DEVICE=chopin
-		export DEFCONFIG=chopin_user_defconfig
+if [ "$1" = "--ares" ]; then
+    export DEVICE=ares
+    export DEFCONFIG=ares_user_defconfig
+elif [ "$1" = "--chopin" ]; then
+    export DEVICE=chopin
+    export DEFCONFIG=chopin_user_defconfig
+elif [ "$1" = "--chopin" ]; then
+     export DEVICE=agate
+     export DEFCONFIG=agate_user_defconfig
+elif [ -z "$1" ]; then
+    export DEVICE=mt6893-common
+    export DEFCONFIG_1=chopin_user_defconfig
+    export DEFCONFIG_2=ares_user_defconfig
+    export DEFCONFIG_3=agate_user_defconfig
 fi
 
-make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG"
-cp -af out/.config arch/arm64/configs/"$DEFCONFIG"
-git add arch/arm64/configs/"${DEFCONFIG}"
-git commit -m "$DEVICE: Sync config with source"
-echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+if [ -z "$1" ]; then
+    make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG_1"
+    cp -af out/.config arch/arm64/configs/"$DEFCONFIG_1"
+
+    make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG_2"
+    cp -af out/.config arch/arm64/configs/"$DEFCONFIG_2"
+
+     make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG_3"
+     cp -af out/.config arch/arm64/configs/"$DEFCONFIG_3"
+
+    git add arch/arm64/configs/"$DEFCONFIG_1"
+    git add arch/arm64/configs/"$DEFCONFIG_2"
+    git add arch/arm64/configs/"$DEFCONFIG_3"
+    git commit -m "$DEVICE: Sync config with source"
+    echo -e "\nSuccessfully regenerated defconfig for $DEVICE"
+else
+    make -j"$(nproc --all)" O=out ARCH=arm64 SUBARCH=arm64 "$DEFCONFIG"
+    cp -af out/.config arch/arm64/configs/"$DEFCONFIG"
+    git add arch/arm64/configs/"$DEFCONFIG"
+    git commit -m "$DEVICE: Sync config with source"
+    echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+fi
